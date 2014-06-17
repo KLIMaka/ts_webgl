78e27e67c61504f4a3ae0902ea0ab4f6fef4ed55
kirill <kirill@adkernel.com>
2014-06-17 17:08:49 +0300

    raven 3d sprites

--- src/raven.ts
+++ src/raven.ts
@@ -1,31 +1,28 @@
 import getter = require('./libs/getter');
 import data = require('./libs/dataviewstream');
+import browser = require('./libs/browser');
 
-function getQueryVariable(variable):any
-{
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if(pair[0] == variable){
-      return pair[1];
-    }
-  }
-  return(false);
-}
-var resnum = getQueryVariable('res');
-var prev = document.createElement('a');
-prev.href='?res='+((resnum|0)+1);
-prev.innerText = 'Next';
-document.body.appendChild(prev);
+var resnum = browser.getQueryVariable('res');
 
-var P = 'resources/raven2/COLORS';
-var R = 'resources/raven2/RES'+resnum;
+var P = 'resources/engines/raven1/COLORS';
+var R = 'resources/engines/raven1/RES'+resnum;
 
 getter.loader
 .load(R)
 .load(P)
 .finish(() => {
+
+var createTmpBuffer = function() {
+  var buffer = [];
+  return function(size:number) {
+    if(buffer.length < size){
+      buffer = new Array<number>(size);
+    }
+    for (var i = 0; i < size; i++)
+      buffer[i] = 0;
+    return buffer;
+  }
+}();
 
 function LZ(r:data.DataViewStream, size:number):number[] {
   var ret = [];
@@ -70,38 +67,97 @@ function read(r:data.DataViewStream, size:number, mod:number):number[] {
   if (mod == 3) {
     return LZ(r, size);
   } else {
-    var ret = Array<number>(size);
+    var ret = createTmpBuffer(size);
     for (var i = 0; i < size; i++)
       ret[i] = r.readUByte();
     return ret;
   }
 }
 
-function createImg(w:number, h:number, data:number[]) {
+function flip(data:number[], off:number, w:number, h:number):number[] {
+  var ret = new Array<number>(w*h);
+  var i = 0;
+  for (var x = 0; x < w; x++) {
+    for (var y = 0; y < h; y++) {
+      ret[x+y*w] = data[off + i++];
+    }
+  }
+  return ret;
+}
+
+function createImage(w:number, h:number, data:number[], off:number, trans:number, pal:number[], dw:boolean=true, isFlip:boolean=false):void {
+  if (isFlip) {
+    var tmp = h;
+    h = w;
+    w = tmp;
+    data = flip(data, off, w, h);
+    off = 0;
+  }
   var canvas:HTMLCanvasElement = document.createElement('canvas');
-  canvas.width = w;
+  canvas.width = dw?w*2:w;
   canvas.height = h;
   var ctx = canvas.getContext('2d');
-  var id = ctx.createImageData(w, h);
+  var id = ctx.getImageData(0, 0, dw?w*2:w, h);
   var idata = id.data;
-  for (var i = 0; i < w*h*4; i++) {
-    idata[i] = data[i]
+  for (var i = 0; i < w*h; i++) {
+    var idx = i * 4 * (dw?2:1);
+    var col = data[off+i];
+    idata[idx + 0] = pal[col*3+0];
+    idata[idx + 1] = pal[col*3+1];
+    idata[idx + 2] = pal[col*3+2];
+    if (col == trans)
+      idata[idx + 3] = 0;
+    else
+      idata[idx + 3] = 255; 
+    if (dw) {
+      idata[idx + 4] = pal[col*3+0];
+      idata[idx + 5] = pal[col*3+1];
+      idata[idx + 6] = pal[col*3+2];
+      if (col == trans)
+        idata[idx + 7] = 0;
+      else
+        idata[idx + 7] = 255;
+    }
   }
   ctx.putImageData(id, 0, 0); 
   document.body.appendChild(canvas);
 }
 
-function createImageData(w:number, h:number, data:number[], off:number, trans:number, pal:number[]):number[] {
-  var idata = new Array<number>(w*h*4);
-  for (var i = 0; i < w*h; i++) {
-    var idx = i * 4;
-    var col = data[off+i] * 3;
-    idata[idx + 0] = pal[col+0];
-    idata[idx + 1] = pal[col+1];
-    idata[idx + 2] = pal[col+2];
-    idata[idx + 3] = 255;
+function read3dSprite(d:number[], pal:number[]) {
+  var arr = new Uint8Array(d);
+  var r = new data.DataViewStream(arr.buffer, true);
+  var w = r.readUShort();
+  var left = r.readUShort();
+  var right = r.readUShort();
+  var up = r.readUShort();
+  var h = r.readUShort();
+  var colOffs = new Array<number>(right-left);
+  for (var i = 0; i < colOffs.length; i++)
+    colOffs[i] = r.readUShort();
+  var img = createTmpBuffer(w*h);
+
+  var pixels = r.mark();
+  for (var i = 0; i< colOffs.length; i++) {
+    r.setOffset(colOffs[i]);
+    var x = r.readUShort();
+    while (x != 0) {
+      var loff = x/2;
+      var roff = r.readShort();
+      var hoff = r.readUShort()/2;
+      var poff = hoff + roff;
+      var rows = loff - hoff;
+      var mark = r.mark();
+      r.setOffset(poff);
+      for (var j = 0; j < rows; j++) {
+        img[(j+hoff)*w+left+i] = r.readUByte();
+      }
+      r.setOffset(mark);
+
+      x = r.readUShort();
+    }
   }
-  return idata;
+
+  createImage(w, h, img, 0, 0, pal);
 }
 
 function readFile(r:data.DataViewStream, pal:number[]) {
@@ -123,9 +179,9 @@ function readFile(r:data.DataViewStream, pal:number[]) {
       var mod = r.readUByte();
       var unknown = r.readUByte();
 
-      var data = read(r,  imgnum*w*h, mod);
+      var data = read(r, imgnum*w*h, mod);
       for (var i = 0; i < imgnum; i++)
-        createImg(w, h, createImageData(w, h, data, i*w*h, trans, pal));
+        createImage(w, h, data, i*w*h, trans, pal);
 
       console.log('sprite x'+imgnum + ' w:'+w+' h:'+h+' mod:'+mod);
       break;
@@ -142,13 +198,28 @@ function readFile(r:data.DataViewStream, pal:number[]) {
       var mod = r.readUByte();
       var unknown = r.readUByte();
 
-      var data = read(r,  imgnum*w*h, mod);
+      var data = read(r, imgnum*w*h, mod);
       for (var i = 0; i < imgnum; i++)
-        createImg(w, h, createImageData(w, h, data, i*w*h, trans, pal));
+        createImage(w, h, data, i*w*h, trans, pal);
 
       console.log('font x'+imgnum + ' w:'+w+' h:'+h+' mod:'+mod);
       break;
     }
+
+    case 6: { //texture
+      var len = r.readUShort();
+      var data = LZ(r, len);
+      createImage(len/0x40, 0x40, data, 0, 0, pal, true, true);
+      break;
+    }
+
+    case 7: { //3D sprite
+      var len = r.readUShort();
+      var data = LZ(r, len);
+      read3dSprite(data, pal);
+      break;
+    }
+
     default: {
       // throw new Error('Wrong type');
       console.log('type = ' + type);
@@ -167,16 +238,20 @@ function readPal(r:data.DataViewStream, off:number):number[] {
   return pal;
 }
 
-var pal = readPal(new data.DataViewStream(getter.get(P), true), 256*3*getQueryVariable('pal') );
+var palnum = browser.getQueryVariable('pal');
+var pal = readPal(new data.DataViewStream(getter.get(P), true), 256*3*palnum);
 var res = new data.DataViewStream(getter.get(R), true);
 var size = res.readUInt();
 console.log("size = " + size);
-for (var i = 0; i < size-1; i++) {
-  var off = res.readUInt();
-  var mark = res.mark();
-  res.setOffset(off);
-  readFile(res, pal);
-  res.setOffset(mark);
+var offsets = new Array<number>(size);
+for (var i = 0; i < size; i++) {
+  offsets[i] = res.readUInt();
+}
+
+for (var i = 0; i < size; i++){
+  res.setOffset(offsets[i]);
+  readFile(res,pal);
+  console.log(i);
 }
 
 });
