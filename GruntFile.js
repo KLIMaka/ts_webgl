a06f619103be86eafe6349033e87f80280ab1d16
kirill <kirill@adkernel.com>
2014-07-04 19:00:31 +0300

    gruntfile

--- GruntFile.js
+++ GruntFile.js
@@ -1,54 +1,31 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-typescript');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.initConfig({});
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    typescript: {
-      raven: {
-        src: ['src/raven.ts'],
-        dest: 'distr/',
-        options: {
-          basePath: 'src/',
-          module: 'amd'
-        }
-      },
-      anvil: {
-        src: ['src/anvil.ts'],
-        dest: 'distr/',
-        options: {
-          basePath: 'src/',
-          module: 'amd'
-        }
-      },
-      build: {
-        src: ['src/build.ts'],
-        dest: 'distr/',
-        options: {
-          basePath: 'src/',
-          module: 'amd'
-        }
-      },
-    },
-    watch: {
-      all: {
-        files: '**/*.ts',
-        tasks: ['typescript:raven', 'typescript:anvil']
-      },
-      raven: {
-        files: '**/*.ts',
-        tasks: ['typescript:raven']
-      },
-      anvil: {
-        files: '**/*.ts',
-        tasks: ['typescript:anvil']
+  var modules = [
+    'raven',
+    'buid',
+    'anvil'
+  ];
+
+  for (var i = 0; i < modules.length; i++) {
+    var module = modules[i];
+
+    grunt.config(['typescript', module], {
+      src: ['src/' + module + '.js'],
+      dest: 'distr/',
+      options: {
+        buildPath: 'src/',
+        module: 'amd'
       }
-    },
-  });
+    });
+    grunt.config(['watch', module], {
+      files: '**/*.ts',
+      tasks: ['typescript:' + module]
+    });
 
-  grunt.registerTask('default', ['watch']);
-  grunt.registerTask('raven', ['typescript:raven']);
-  grunt.registerTask('anvil', ['typescript:anvil']);
-  grunt.registerTask('build', ['typescript:build']);
+    grunt.registerTask(module, ['typescript:' + module]);
+  }
  
 }
