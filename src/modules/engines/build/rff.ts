a2dd3a293781aac1ca5983ea53cb30926148b003
kirill <kirill@adkernel.com>
2015-06-02 17:02:37 +0300

    fff

--- src/modules/engines/build/rff.ts
+++ src/modules/engines/build/rff.ts
@@ -26,10 +26,10 @@ export class RffFile {
 
   constructor(buf:ArrayBuffer) {
     this.data = new data.DataViewStream(buf, true);
-    this.header = headerStruct(this.data);
+    this.header = headerStruct.read(this.data);
     this.data.setOffset(this.header.offFat);
     var len = this.header.numFiles * 48;
-    var fat = data.array(data.ubyte, len)(this.data);
+    var fat = data.array(data.ubyte, len).read(this.data);
     if (this.header.version >= 0x301) {
       var key = this.header.offFat & 0xff;
       for (var i = 0; i < len; i+=2) {
@@ -44,7 +44,7 @@ export class RffFile {
   }
 
   private loadFat(stream:data.DataViewStream, numFiles:number):void {
-    this.fat = data.structArray(numFiles, fatRecord)(stream);
+    this.fat = data.array(fatRecord, numFiles).read(stream);
     for (var i in this.fat) {
       var r = this.fat[i];
       r.filename = this.convertFname(r.filename);
@@ -59,7 +59,7 @@ export class RffFile {
   public get(fname:string):Uint8Array {
     var record = this.fat[this.namesTable[fname]];
     this.data.setOffset(record.offset);
-    var arr = data.array(data.ubyte, record.size)(this.data);
+    var arr = data.array(data.ubyte, record.size).read(this.data);
     if (record.flags & 0x10)
       for (var i = 0; i < 256; i++)
         arr[i] ^= (i >> 1);
