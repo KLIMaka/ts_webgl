3bb771f366321f92247a55ae2d9ae836d77c54a9
KLIMaka <klimaka01@gmail.com>
2019-10-22 00:01:14 +0300

    glmatrix

--- src/libs_js/glmatrix.js
+++ src/libs_js/glmatrix.js
@@ -1,13 +1,13 @@
-import * as glMatrix from "./common.js";
-import * as mat2 from "./mat2.js";
-import * as mat2d from "./mat2d.js";
-import * as mat3 from "./mat3.js";
-import * as mat4 from "./mat4.js";
-import * as quat from "./quat.js";
-import * as quat2 from "./quat2.js";
-import * as vec2 from "./vec2.js";
-import * as vec3 from "./vec3.js";
-import * as vec4 from "./vec4.js";
+import * as glMatrix from "./glmatrix/common.js";
+import * as mat2 from "./glmatrix/mat2.js";
+import * as mat2d from "./glmatrix/mat2d.js";
+import * as mat3 from "./glmatrix/mat3.js";
+import * as mat4 from "./glmatrix/mat4.js";
+import * as quat from "./glmatrix/quat.js";
+import * as quat2 from "./glmatrix/quat2.js";
+import * as vec2 from "./glmatrix/vec2.js";
+import * as vec3 from "./glmatrix/vec3.js";
+import * as vec4 from "./glmatrix/vec4.js";
 
 export {
   glMatrix,
